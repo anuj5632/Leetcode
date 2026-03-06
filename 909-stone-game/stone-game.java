6aae61e317945feff2437f12a812dbaaecdda804
Anuj Chandrakar <anuj.chandrakar99@gmail.com>
2026-03-06 09:33:14 +0530

    Time: 5 ms (13.73%) | Memory: 43.3 MB (37.66%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,17 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int countA = 0;
+        int countB = 0;
+        int n = piles.length;
+        Arrays.sort(piles);
+        for(int i = n-1;i>=0;i--){
+            if(n % 2 == 0){
+                countA += piles[i];
+            }
+            else{
+                countB += piles[i];
+            }
+        }
+        return countA > countB;
     }
 }
