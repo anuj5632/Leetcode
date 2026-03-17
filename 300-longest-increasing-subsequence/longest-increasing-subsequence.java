90ae26aad0683052a8d77faa9172f7543f64a650
Anuj Chandrakar <anuj.chandrakar99@gmail.com>
2026-03-17 07:47:11 +0530

    Time: 404 ms (6.51%) | Memory: 237 MB (16.81%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,21 +1,26 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        List<Integer> lis = new ArrayList<>();
-        
-        for (int num : nums) {
-            int index = Collections.binarySearch(lis, num);
-            
-            if (index < 0) {
-                index = -index - 1;
-            }
-            
-            if (index == lis.size()) {
-                lis.add(num);
-            } else {
-                lis.set(index, num);
-            }
+    public int func(int ind,int prev_ind,int n,int[] nums,int[][] dp){
+        int len = 0;
+        if(ind == n){
+            return 0;
         }
-        
-        return lis.size();
+        if(dp[ind][prev_ind + 1] != -1){
+            return dp[ind][prev_ind+1];
+        }
+        len = 0 + func(ind+1,prev_ind,n,nums,dp);
+        if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
+            len = Math.max(len,1+func(ind+1,ind,n,nums,dp));
+        }
+        return dp[ind][prev_ind+1] = len;
+    }
+    public int lengthOfLIS(int[] nums) {
+       int n = nums.length;
+       int[][] dp = new int[n][n];
+       for(int i = 0;i<n;i++){
+          for(int j = 0;j<n;j++){
+            dp[i][j] = -1;
+        }
+       }
+       return func(0,-1,n,nums,dp);
     }
 }
