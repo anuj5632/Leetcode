9021b2d9b09f14c2b307c9604da68ad80afa9e00
Anuj Chandrakar <anuj.chandrakar99@gmail.com>
2026-03-27 07:20:06 +0530

    Time: 1 ms (99.47%) | Memory: 47.1 MB (41.60%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -2,36 +2,10 @@ class Solution {
     public boolean areSimilar(int[][] mat, int k) {
         int m = mat.length;
         int n = mat[0].length;
-        int shifts = k % n;
-
-        int[][] clone = new int[m][n];
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                clone[i][j] = mat[i][j];
-            }
-        }
-        while(shifts-- > 0){
-            for(int i = 0;i<m;i++){
-                if(i % 2 == 0){
-                    int first = mat[i][0];
-                    for(int j = 0;j<n-1;j++){
-                        mat[i][j] = mat[i][j+1];
-                    }
-                    mat[i][n-1] = first;
-                }
-                else{
-                    int last = mat[i][n-1];
-                    for(int j = n-1;j>0;j--){
-                        mat[i][j] = mat[i][j-1]; 
-                    }
-                    mat[i][0] = last;
-                }
-            }
-
-        }
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(mat[i][j] != clone[i][j]){
+                int ind = (j+k)%n;
+                if(mat[i][j] != mat[i][ind]){
                     return false;
                 }
             }
