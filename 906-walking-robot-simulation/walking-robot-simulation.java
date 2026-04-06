69d3e4a39bb39361d0c1da9d36a49aea0a82df92
Anuj Chandrakar <anuj.chandrakar99@gmail.com>
2026-04-06 19:21:40 +0530

    Time: 35 ms (52.90%) | Memory: 52.6 MB (32.61%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -1,44 +1,71 @@
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-
-        HashSet<Long> set = new HashSet<>();
-
-        for (int[] o : obstacles) {
-            long key = ((long)o[0] << 32) | (o[1] & 0xffffffffL);
-            set.add(key);
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i<obstacles.length;i++){
+            set.add(obstacles[i][0]+","+obstacles[i][1]);
         }
-
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
-
-        int x = 0, y = 0, dir = 0;
-        int maxDist = 0;
-
-        for (int cmd : commands) {
-
-            if (cmd == -1) {
-                dir = (dir + 1) % 4; 
-            } 
-            else if (cmd == -2) {
-                dir = (dir + 3) % 4; 
-            } 
-            else {
-                for (int i = 0; i < cmd; i++) {
-                    int nx = x + dx[dir];
-                    int ny = y + dy[dir];
-
-                    long key = ((long)nx << 32) | (ny & 0xffffffffL);
-
-                    if (set.contains(key)) break;
-
-                    x = nx;
-                    y = ny;
-
-                    maxDist = Math.max(maxDist, x*x + y*y);
+        int x=0;
+        int y=0;
+        int max=0;
+        char ch='N';
+        for(int i=0;i<commands.length;i++){
+            int curr=commands[i];
+            if(curr==-1){
+                if(ch=='N'){
+                    ch='E';
+                }
+                else if(ch=='E'){
+                    ch='S';
+                }
+                else if(ch=='S'){
+                    ch='W';
+                }
+                else{
+                    ch='N';
                 }
             }
-        }
-
-        return maxDist;
+            if(curr==-2){
+                if(ch=='N'){
+                    ch='W';
+                }
+                else if(ch=='W'){
+                    ch='S';
+                }
+                else if(ch=='S'){
+                    ch='E';
+                }
+                else{
+                    ch='N';
+                }
+            }
+            else{
+                if(ch=='N'){
+                    for(int j=0;j<curr;j++){
+                        if(set.contains(x+","+(y+1))) break;
+                        y++;
+                    }
+                }
+                if(ch=='S'){
+                    for(int j=0;j<curr;j++){
+                        if(set.contains(x+","+(y-1))) break;
+                        y--;
+                    }
+            }
+                if(ch=='W'){
+                    for(int j=0;j<curr;j++){
+                        if(set.contains((x-1)+","+y)) break;
+                        x--;
+                    }
+                 }
+                 if(ch=='E'){
+                    for(int j=0;j<curr;j++){
+                        if(set.contains((x+1)+","+y)) break;
+                        x++;
+                    }
+                 }
+            }
+                 max=Math.max(max,(int)(Math.pow(x,2) + Math.pow(y,2)));
+            }
+            return max;
     }
 }
