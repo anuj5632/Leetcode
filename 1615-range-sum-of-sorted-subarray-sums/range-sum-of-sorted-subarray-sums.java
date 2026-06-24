4c8076f51d68a7cdf1a41af7d780308f385e3802
Anuj Chandrakar <anuj.chandrakar99@gmail.com>
2026-06-24 08:53:26 +0530

    Time: 217 ms (32.50%) | Memory: 83.7 MB (36.67%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int MOD = 1000000007;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+                list.add(sum);
+            }
+        }
+        Collections.sort(list);
+        long ans = 0;
+        for(int i = left - 1; i < right; i++){
+            ans = (ans + list.get(i)) % MOD;
+        }
+
+        return (int)ans;
+    }
+}
