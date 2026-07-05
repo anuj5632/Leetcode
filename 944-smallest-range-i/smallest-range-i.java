68e78b605e866de24d4532e8ebbc63484b48a349
Anuj Chandrakar <anuj.chandrakar99@gmail.com>
2026-07-05 10:48:48 +0530

    Time: 3 ms (74.11%) | Memory: 47.4 MB (21.32%) - LeetSync

---
+++ 944-smallest-range-i/smallest-range-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        int min = nums[0];
+        int max = nums[0];
+
+        for(int i : nums){
+            min = Math.min(i,min);
+            max = Math.max(i,max);
+        }
+
+        return Math.max(0,max-min-2*k);
+    }
+}
