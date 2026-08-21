a956c3184ed738d4881b8ee0071f78bd9679a1c5
Anuj Chandrakar <anuj.chandrakar99@gmail.com>
2026-08-21 18:50:43 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (25.10%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+           int length[] = new int[1001];
+
+           for(int trip[] : trips){
+            length[trip[1]] += trip[0];
+            length[trip[2]] -= trip[0];
+           }
+
+           for(int passenger : length){
+            capacity -= passenger;
+            if(capacity < 0){
+                return false;
+            }
+           }
+           return true;
+    }
+}
